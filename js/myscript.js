c45248ca5cefb404c83c8494377fa2f16d1852ad
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-27 15:54:00 +0200

    feat: add random email list

--- js/myscript.js
+++ js/myscript.js
@@ -3,11 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            myEmails: [],
         }
     },
-    methods: {
-        
 
+    mounted() {
+        // ciclo per creare 10 indirizzi email
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((risposta) => {
+                const result = risposta.data.response;
+                // pusho email in array vuoto
+                this.myEmails.push(result);
+            })
+        }
     }
 }).mount('#app');
